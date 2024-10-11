dae0f54bbca8fc517234433bf42ffdeaa3eff4e2
Abhijit Kumar <abhijitkumar10.5@gmail.com>
2024-10-11 07:32:44 +0530

    lesson 02

---
+++ lesson02/build/js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+let myName = 'Abhijit';
+let meaningOfLife;
+let isLoading;
+let album;
+myName = 'John';
+meaningOfLife = 42;
+isLoading = false;
+album = 1984;
+album = 'Van Halen';
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
---
+++ lesson02/src/main.ts
@@ -0,0 +1,19 @@
+let myName: string = 'Abhijit'
+let meaningOfLife: number
+let isLoading: boolean
+let album: any
+
+myName = 'John'
+meaningOfLife = 42
+isLoading = false
+album = 1984
+album = 'Van Halen'
+
+const sum = (a: number, b: number) => {
+  return a + b
+}
+
+let postId: string | number
+let isActive: number | boolean
+
+let re: RegExp = /\w+/g
